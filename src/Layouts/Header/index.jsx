1be322b8566aa00f40f52c4835bc62967ea4bfbe
ngokhang <ngokhang2002vn@gmail.com>
2023-04-04 11:52:51 +0700

    first commit

---
+++ src/Layouts/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Header } from 'antd/es/layout/layout';
+
+HeaderLayout.propTypes = {};
+
+function HeaderLayout(props) {
+  return <Header></Header>;
+}
+
+export default HeaderLayout;
