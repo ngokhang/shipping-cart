fc3e732d8f47d73a96e917553e4247e393ffd101
ngokhang <ngokhang2002vn@gmail.com>
2023-04-09 08:37:31 +0700

    resolve conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Layout } from 'antd';
-<<<<<<< HEAD
 import React from 'react';
 import MyHeader from './components/MyHeader';
 
@@ -8,18 +7,6 @@ function App() {
     <Layout>
       <MyHeader />
     </Layout>
-=======
-import { Content } from 'antd/es/layout/layout';
-import React from 'react';
-import './App.scss';
-import HeaderLayout from './Layouts/Header';
-import Footer from './Layouts/footer';
-
-
-function App() {
-  return (
-    <Footer/>
->>>>>>> 266c15bafa39e68158d644fabc58afee89578af5
   );
 }
 
