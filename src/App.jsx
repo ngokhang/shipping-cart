f936a988711fe509b8cf56a20c75e3e3ced67a92
ngokhang <ngokhang2002vn@gmail.com>
2023-05-30 15:23:52 +0700

    sửa lỗi không lấy ds sản phẩm lần render đầu

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { Layout } from 'antd';
 import { Content } from 'antd/es/layout/layout';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import './App.scss';
 import FooterLayout from './Layouts/Footer';
 import './assets/styles/index.scss';
 import MyHeader from './components/MyHeader';
-import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
--- src/components/MyHeader/HeaderCart/components/FilledCart.jsx
+++ src/components/MyHeader/HeaderCart/components/FilledCart.jsx
@@ -1,11 +1,22 @@
-import { Button } from 'antd';
-import React, { useMemo } from 'react';
+import { Button, Spin } from 'antd';
+import React, { useContext, useEffect } from 'react';
 import { formatter } from '../../../../shared/constants';
 import './FilledCart.scss';
 import ItemCart from './ItemCart';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchOrdereList } from '../../../../features/orders/orderSlide';
+import { Context } from '../../../../store/Context';
 
 function FilledCart(props) {
-  const { _orderList } = props;
+  const { _orderList, quantityOrders } = props;
+  const loading = useSelector(state => state.orders.isLoading);
+  const userId = localStorage.getItem('userId');
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchOrdereList(userId));
+    console.log('filledd cart');
+  }, [dispatch, userId, quantityOrders])
 
   const calcTotalPrice = () => {
     return _orderList.filter(item => {
@@ -21,10 +32,10 @@ function FilledCart(props) {
     <>
       <ul className='list-orders'>
         {
-          _orderList.filter(item => {
+          _orderList?.filter(item => {
             if (item.attributes.product.data !== null) return true;
             return false;
-          }).map((item, index) => (
+          })?.map((item, index) => (
             <li>
               <ItemCart name={item.attributes.product.data.attributes.name} imgUrl={item.attributes.product.data.attributes.image} price={item.attributes.product.data.attributes.price} quantityProduct={item.attributes.quantity} orderId={item.id} key={index} indexInArray={index} />
             </li>
@@ -40,6 +51,7 @@ function FilledCart(props) {
       <Button className="btn-checkout" block>
         CHECKOUT
       </Button>
+
 
     </>
   );
